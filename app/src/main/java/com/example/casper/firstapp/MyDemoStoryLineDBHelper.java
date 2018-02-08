79d5296bd0f1bc24c2a52c8c393a217b42a8d818
Wouter Coudenys <wouter.coudenys@hotmail.com>
2018-02-08 11:10:29 +0100

    walking arounds and completing sequential challenges test works

--- app/src/main/java/com/example/casper/firstapp/MyDemoStoryLineDBHelper.java
+++ app/src/main/java/com/example/casper/firstapp/MyDemoStoryLineDBHelper.java
@@ -10,51 +10,54 @@ import cz.mendelu.busItWeek.library.builder.StoryLineBuilder;
 public class MyDemoStoryLineDBHelper extends StoryLineDatabaseHelper {
 
     public MyDemoStoryLineDBHelper() {
-        super(18);
+        super(32);
     }
 
     @Override
     protected void onCreate(StoryLineBuilder builder) {
-        builder.addGPSTask("1")
-                .radius(1000)
-                .location(49.209543, 16.614235)
-                .victoryPoints(5)
+//        builder.addGPSTask("Mushrooms")
+//                .radius(10)
+//                .location(49.210346, 16.616325)
+//                .victoryPoints(5)
+//                .choicePuzzle()
+//                .question("Which musroom is poisonous?")
+//                .addChoice("Brussels",false)
+//                .addChoice("Brno",true)
+//                .addChoice("Paris",false)
+//                .addChoice("London",false)
+//                .puzzleDone()
+//                .taskDone();
+////        builder.addBeaconTask("2")
+////                .location(49.210451, 16.614749)
+////                .beacon(5635,2481)
+////                .choicePuzzle()
+////                .question("What is the best city in the world?")
+////                .addChoice("Brussels",false)
+////                .addChoice("Brno",true)
+////                .addChoice("Paris",false)
+////                .addChoice("London",false)
+////                .puzzleDone()
+////                .taskDone();
+////
+
+        builder.addGPSTask("SWORDS")
+                .radius(500)
+                .location(49.209871, 16.614903)
                 .simplePuzzle()
-                .question("What is the answe to life and everything?")
-                .answer("42")
-                .hint("hitchhikers guide...")
-                .puzzleTime(30000)
-                .puzzleDone()
-                .taskDone();
-        builder.addBeaconTask("2")
-                .location(49.210451, 16.614749)
-                .beacon(5635,2481)
-                .choicePuzzle()
-                .question("What is the best city in the world?")
-                .addChoice("Brussels",false)
-                .addChoice("Brno",true)
-                .addChoice("Paris",false)
-                .addChoice("London",false)
+                .question("How many swords do you see?")
+                .answer("33")
                 .puzzleDone()
                 .taskDone();
 
-        builder.addGPSTask("3")
-                .location(49.210451, 16.614749)
-                .radius(50000)
+        builder.addGPSTask("MUSHROOM")
+                .radius(15)
+                .location(49.210158, 16.614592)
                 .imageSelectPuzzle()
-                .question("Select image?")
-                .addImage(R.drawable.foxes_1, false)
-                .addImage(R.drawable.foxes_2, false)
-                .addImage(R.drawable.foxes_3, false)
-                .addImage(R.drawable.foxes_4, true)
-                .puzzleDone()
-                .taskDone();
-        builder.addCodeTask("4")
-                .qr("QRcode1")
-                .location(48.210451, 20.614749)
-                .simplePuzzle()
-                .question("Who am I?")
-                .answer("Casper")
+                .question("Which mushroom is the poisonous one?")
+                .addImage(R.drawable.mushroom_black, false)
+                .addImage(R.drawable.mushroom_amanita, true)
+                .addImage(R.drawable.mushroom_boletus, false)
+                .addImage(R.drawable.mushroom_white, false)
                 .puzzleDone()
                 .taskDone();
 
